3560ea5a06f34d5665a3fb05cebbe9671ebb8cfb
Shaun Ericson <shaun.ericson@outlook.com>
2018-09-22 12:39:48 -0700

    final build

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -3,16 +3,8 @@
 var giphyMenu = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish"];
 var giphyToggle =
     [{
-        // id: 0,
-        // still: true
+        
     }];
-
-// var giphyToggle = {
-//     id: 0, 
-//             {
-//                 still: true
-//             }
-//         }
 
 const user = {
     id: 101,
@@ -57,49 +49,31 @@ $("#submitAddAnimal").on("click", function () {
 
 $(document.body).on("click", ".menu-item", function () {
 
+    // Set this to a variable
     var q = $(this).text();
 
-    // var giphyToggle = Object.create(null);
-    // giphyToggle.id = 0; // "name" is a property set on "me", but not on "person"
-    // giphyToggle.toggle = false; // inherited properties can be overwritten
-    // console.log(giphyToggle);
-
+    // Get API giphy images
     $.ajax({
         url: "https://api.giphy.com/v1/gifs/search?q=" + q +
             "&api_key=FMbq2Vtj5T4zKhfSVr9wDCL3ttYknL1H&limit=10",
         method: "GET"
-    }).then(function (res) {
-        console.log(res);
-        // console.log(res.data.length);    
+    }).then(function (res) {    
         var giphyResponse = res.data;
-        // console.log(giphyResponse);
 
         // Empty giphy DIV
         $("#giphyPics").empty();
 
         // For loop to go through response and output data / pics
         for (x = 0; x < giphyResponse.length; x++) {
-            // console.log("for loop working");
 
             // Assign Still and Moving giphy URLs to variables
             var giphyPicURLStill = giphyResponse[x].images.fixed_width_still.url;
             var giphyPicUrlMove = giphyResponse[x].images.fixed_width.url;
-            // console.log(giphyPicURLStill);
-            // console.log(giphyPicUrlMove);
 
             //Create P tag for giphy rating
             var p = $("<p>");
             p.text("Rating: " + giphyResponse[x].rating);
             var b = $("<b>");
-            // console.log(giphyResponse[x].rating);
-            // console.log(p);
-            // $("#giphyPics").append("<p>Rating:</p>");
-            // var pRating = $("<p>");
-            // pRating.text(giphyResponse[x].rating);
-            // console.log(giphyResponse[x].rating);
-            // console.log(pRating.text());
-            // $("#giphyPics").append(pRating);
-            // $("#giphyPics").append("<br>");
 
             //Create img tag for giphy URL
             var i = $("<img>");
@@ -108,14 +82,11 @@ $(document.body).on("click", ".menu-item", function () {
                 .attr("imageStill", giphyPicURLStill)
                 .attr("imageMove", giphyPicUrlMove)
                 .attr("imageId", giphyResponse[x].id);
-            // $("#giphyPics").append(i);
 
-            // Create object to capture toggle of giphy URl still vs. move
+            // Push object to capture toggle of giphy URl still vs. move
             giphyToggle.push({ id: giphyResponse[x].id, still: true });
-            // giphyToggle.id[x] = ({ giphyResponse.id: giphyResponse[x].id, still: true });
-            // giphyToggle
 
-            // Create DIV
+            // Create DIV and append elements
             var d = $("<div>");
             d.attr("class", "col-lg-4");
             d.append(p);
@@ -124,12 +95,10 @@ $(document.body).on("click", ".menu-item", function () {
 
         }
 
-        console.log(giphyToggle);
-
-        // var toggle = 0;
-
+        // Event handler for setting still vs. moving images
+        
         $(".giphyImage").on("click", function () {
-           console.log("yo")
+
             var imageStill = $(this).attr("imageStill");
             var imageMove = $(this).attr("imageMove");
             var imageId = $(this).attr("imageId");
@@ -147,32 +116,6 @@ $(document.body).on("click", ".menu-item", function () {
                     }
                 }
             }
-            // if (giphyToggle.imageId.still === true) {
-
-            // }
-            // if (giphyToggle[x].toggle.still === true) {
-            //     $(this).attr("src", imageMove);
-            //     giphyToggle.imageId.toggle.still = false;
-            // } else {
-            //     $(this).attr("src", imageStill);
-            //     giphyToggle.imageId.toggle.still = true;
-            // }
         });
     });
 });
-
-// const someArray = [
-//     { id: 1, name: "susie"},
-//     { id: 2, name: "mark"},
-//     { id: 3, name: "wendy"},
-//     { id: 4, name: "sal"},
-//     { id: 5, name: "yo"},
-//     { id: 6, name: "lo"},
-//   ]
-
-//   const getPropById = (id) => {
-//     const [object = { id: id }] = someArray
-//     return object
-//   }
-
-//   console.log(getPropById(1))
